07d7606ff429364ae8fba19c1bc9a6fad8631b61
prabhakarsinghssm1@gmail.com <prabhakarsinghssm1@gmail.com>
2024-03-05 14:19:01 +0530

    cluster set fot MongoDB

--- To-Do/server/src/utils/constants.js
+++ To-Do/server/src/utils/constants.js
@@ -1,10 +1,10 @@
 //connecting to a MongoDB instance running on the cloud
-//export const DB_CONNECT="mongodb+srv://acheiveer:8Ph9CkUqrh5EvHQf@cluster0.lqjo4jx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+export const DB_CONNECT="mongodb+srv://acheiveer:8Ph9CkUqrh5EvHQf@cluster0.lqjo4jx.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0";
 
 
 // connecting to a MongoDB instance running on the local machine
 //export const DB_CONNECT="mongodb://localhost:27017/todolist";  ---  This URL is not working for my system to connenct mongoDB on local machine
-export const DB_CONNECT="mongodb://127.0.0.1:27017/todolist";   //-- This URL is working properly
+//export const DB_CONNECT="mongodb://127.0.0.1:27017/todolist";   //-- This URL is working properly
 
 
 
